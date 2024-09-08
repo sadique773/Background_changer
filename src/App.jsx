1f418b8653771eab5220d73fe9d9a3b15389087a
Sadique Hussain <106325596+sadique773@users.noreply.github.com>
2024-09-08 12:50:20 +0530

    Update UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,23 @@ function App() {
   const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <div className='h-screen w-screen relative' style={{backgroundColor:color}}>
-        <div className='bg-white rounded-3xl  absolute bottom-5 grid grid-cols- gap-4 content-start ...' >
-          <button className='m-1 rounded-2xl bg-red-600' onClick={()=>setColor("red")}>Red</button>
-          <button className='m-1 rounded-2xl bg-green-600'onClick={()=>setColor("green")}>Green</button>
-          <button className='m-1 rounded-2xl bg-blue-600'
-          onClick={()=>setColor("blue")}>Blue</button>
-          <button className='m-1 rounded-2xl bg-amber-600'
-          onClick={()=>setColor("olive")}>Olive</button>
-          <button className='m-1 rounded-2xl bg-gray-600'onClick={()=>setColor("gray")}>Gray</button>
-          <button className='m-1 rounded-2xl bg-yellow-400 text-black' onClick={()=>setColor("yellow")}>Yellow</button>
-          <button className='m-1 rounded-2xl bg-red-300'onClick={()=>setColor("pink")}>Pink</button>
-          <button className='m-1 rounded-2xl bg-white text-black border-black'onClick={()=>setColor("white")}>White</button>
-          <button className='m-1 rounded-2xl bg-black text-white'onClick={()=>setColor("black")}>Black</button>
+
+      <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
+       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+       <div className="flex flex-wrap justify-center gap-1 shadow-lg bg-white px-3 py-2 rounded-3xl" >
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg0" style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>Green</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>Blue</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg0" style={{backgroundColor:"olive"}} onClick={()=>setColor("olive")}>Olive</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'gray'}} onClick={()=>setColor("gray")}>Gray</button>
+          <button className="outline-none px-4 py-1 rounded-full shadow-lg00 text-black" style={{backgroundColor:'yellow'}} onClick={()=>setColor("yellow")}>Yellow</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"pink"}} onClick={()=>setColor("pink")}>Pink</button>
+          <button className="outline-none px-4 py-1 rounded-full text-black shadow-lgxt-black border-black" style={{backgroundColor:"white"}} onClick={()=>setColor("white")}>White</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lgxt-white" style={{backgroundColor:'black'}} onClick={()=>setColor("black")}>Black</button>
         </div>
+       </div>
       </div>
-    </>    
+  
   )
 }
 
